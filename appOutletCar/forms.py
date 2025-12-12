272ca6414fcb36302a41c69bcb585a2bdf9168cd
Ander Ormazabal <ander.ormazabal@opendeusto.es>
2025-12-12 12:57:15 +0100

    Cambios de Alberto

--- appOutletCar/forms.py
+++ appOutletCar/forms.py
@@ -4,7 +4,7 @@ from .models import Coche, Oferta, Categoria
 class OutlerCarModelForm(forms.ModelForm):
     class Meta:
         model = Coche
-        fields = ['matricula', 'idMarca','modelo', 'anio', 'idPrecio', 'idCombustible', 'idCat', 'foto']
+        fields = ['matricula', 'idMarca','modelo', 'anio', 'km', 'idCombustible', 'idCat', 'foto']
 
 
         
--- appOutletCar/views_forms.py
+++ appOutletCar/views_forms.py
@@ -1,6 +1,6 @@
 from .forms import OutlerCarModelForm
 from django.shortcuts import render
-from .models import Coche
+from .models import Coche, Oferta
 
 def formulario_coche(request):
 
@@ -21,18 +21,23 @@ def procesar_form_coche(request):
             marca = form.cleaned_data['idMarca']
             modelo = form.cleaned_data['modelo']
             anio = form.cleaned_data['anio']
-            precio = form.cleaned_data['idPrecio']
             combustible = form.cleaned_data['idCombustible']
             categoria = form.cleaned_data['idCat']
             foto = form.cleaned_data['foto']
+            km = form.cleaned_data['km']
 
-            new_coche = Coche(matricula = matricula, idMarca = marca, modelo = modelo, anio = anio, idPrecio = precio, idCombustible = combustible, foto = foto)
+            precio_valor = request.POST.get('precio')
+            precio = Oferta(precio=precio_valor)
+            precio.save()
+
+            new_coche = Coche(matricula = matricula, idMarca = marca, modelo = modelo, anio = anio, idPrecio = precio, km = km, idCombustible = combustible, foto = foto)
             new_coche.save()
 
             new_coche.idCat.set(categoria)
             print(f"Nuevo coche guardado: \nMatricula: {matricula}, \nMarca: {marca}, \nModelo: {modelo}, \nAnio: {anio}, \nPrecio: {precio}, \nCombustible: {combustible}, \nCategoria: {categoria}")
             return render(request, 'form_coche.html', {'form': OutlerCarModelForm()})
 
+        
         else:
             print(f"Errores del formulario: {form.errors}")
             return render(request, 'form_coche.html', {'form': form})
